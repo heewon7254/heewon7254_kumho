127a0ab1857795c6c7ec0eb2d562035b490649f9
heewon7254 <heewon7254@naver.com>
2022-06-23 17:03:33 +0900

    hamburger button

--- js/common.js
+++ js/common.js
@@ -8,53 +8,50 @@ $(function(){
     subMenu = menu.find('ul');
 
 
-    // $(window).scroll(function(){
-    //     if($(this).scrollTop()>0){
-    //         if(!header.hasClass('shrink')){
-    //             header.addClass('shrink');
-    //         }
-    //     }
-    //     else{
-    //         if(header.hasClass('shrink')){
-    //             header.removeClass('shrink');
-    //         }
-    //     }
-
-    // });
-
-    subMenu.each(function(){
-        if($(this).outerHeight() > newHeight){
-            newHeight =$(this).outerHeight() + headerHeight;
-        }
-    });
-
-    menu.hover(
-        function(){
-            header.stop().animate({height:newHeight});
-        },
-        function(){
-            header.stop().animate({height:headerHeight});
-        }
-    );
+    if(!$('nav').hasClass('active')){
+        subMenu.each(function(){
+            if($(this).outerHeight() > newHeight){
+                newHeight =$(this).outerHeight() + headerHeight;
+            }
+        });
+    
+        menu.hover(
+            function(){
+                header.stop().animate({height:newHeight});
+            },
+            function(){
+                header.stop().animate({height:headerHeight});
+            }
+        );
+    }
 
     
+        menu.each(function(){
+            $(this).mouseover(function(){
+                let width = $(this).outerWidth();
+                let height =$(this).outerHeight();
+                let left = $(this).offset().left;
+    
+                $('.target').css({
+                    width:`${width}px`,
+                    top:`${height - 20}px`,
+                    left:`${left}px`,
+                    opacity:1
+                })
+            $(this).mouseout(function(){
+                $('.target').css({opacity:0})
+            });
+            });
+        }); //sub menu
 
-    menu.each(function(){
-        $(this).mouseover(function(){
-            let width = $(this).outerWidth();
-            let height =$(this).outerHeight();
-            let left = $(this).offset().left;
 
-            $('.target').css({
-                width:`${width}px`,
-                top:`${height - 20}px`,
-                left:`${left}px`,
-                opacity:1
-            })
-        $(this).mouseout(function(){
-            $('.target').css({opacity:0})
-        });
-        });
-    }); //sub menu
+    $(function(){
+        $('.hamburger-button').click(function(e){
+        e.preventDefault();
+        $('nav').toggleClass('active');
+        $(this).toggleClass('active');
+      });
+    });
+    
 
 });
